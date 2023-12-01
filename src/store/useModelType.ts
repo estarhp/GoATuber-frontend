bef1a579b78af78f9d83a1fb0fa72b67908642aa
estarhp <3136551640@qq.com>
2023-12-01 22:09:37 +0800

    feat: 增加git TODO

--- src/store/useModelType.ts
+++ src/store/useModelType.ts
@@ -1,5 +1,7 @@
 import {create} from "zustand";
 
+//TODO: 计划删除该store 挂载到相应的对象上
+
 const useModelType = create<ModelTypeStore>((set) => ({
     modelType: undefined,
     setModelType: (newModelType:string | undefined)=> set(()=>({modelType: newModelType}))
--- src/store/useWebSocket.ts
+++ src/store/useWebSocket.ts
@@ -1,5 +1,7 @@
 import { create } from 'zustand'
 
+//TODO 计划删除状态(与刷新视图没有关系)
+
 const useWebSocket = create((set) => ({
     websocket: WebSocket,
     setWebSocket: (newWebSocket: WebSocket) => set(() => ({ websocket: newWebSocket })),
